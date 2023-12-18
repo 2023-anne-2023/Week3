24d8d833dbbb0d1fd9f552715f98c8d01c8a746b
2023-anne-2023 <152497939+2023-anne-2023@users.noreply.github.com>
2023-12-18 05:18:38 +0000

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const thumbnails = document.querySelectorAll(".thumbnails img");
 const displayImage = document.getElementById("displayImage");
+let speech =new SpeechSynthesisUtterance();
 
 thumbnails.forEach(function (thumb) {
   thumb.addEventListener("click", function () {
 
     displayImage.src = thumb.src;
     displayImage.alt = thumb.alt;
-  });
+    speech.text=displayImage.alt;
+    window.speechSynthesis.speak(speech);
+    //const utterThis = new SpeechSynthesisUtterance(speech);
+    //utterThis.rate = rate.value;
+    //synth.speak(utterThis);
+    //play.addEventListener("click", speak);
+    });
 });
+const input = document.querySelector("input");
+const log = document.getElementById("log");
+
+input.addEventListener("keyup", logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+ 
+ }
+ speech.text=log;
+ window.speechSynthesis.speak(log);
